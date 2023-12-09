3573ba966af2b12b8fdb73ed69f3ccd94bf34127
Gibsongf <maccgiiibson@gmail.com>
2023-12-09 07:32:10 -0300

    change file type

---
+++ src/Api.js
@@ -0,0 +1,152 @@
+export async function apiLogin(loginData) {
+    const url = "http://localhost:3000/users/login";
+    // console.log(loginData);
+    try {
+        const response = await fetch(url, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(loginData),
+        });
+
+        if (response.status === 200) {
+            const data = await response.json();
+            console.log("Login successfully");
+            localStorage.setItem("token", data.token);
+            return true;
+        } else {
+            throw new Error();
+        }
+    } catch (error) {
+        throw Error(error);
+    }
+}
+export async function apiRegister(loginData) {
+    const url = "http://localhost:3000/users/register";
+    // console.log(loginData);
+    try {
+        const response = await fetch(url, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(loginData),
+        });
+
+        if (response.status === 200) {
+            const data = await response.json();
+            console.log("Login successfully");
+            localStorage.setItem("token", data.token);
+            return true;
+        } else {
+            throw new Error();
+        }
+    } catch (error) {
+        throw Error(error);
+    }
+}
+async function setupFetch(url, reqMethod = "get", body) {
+    if (!localStorage["token"]) {
+        await apiLogin();
+        // localStorage.setItem("token", data.token);
+    }
+
+    const reqConfig = {
+        method: reqMethod,
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: "Bearer " + localStorage["token"],
+        },
+    };
+    if (body) {
+        reqConfig.body = JSON.stringify(body);
+    }
+    try {
+        const response = await fetch(url, reqConfig);
+        if (response.status === 200 || response.status === 400) {
+            const data = await response.json();
+            return data;
+        }
+
+        if (response.status === 401) {
+            localStorage.removeItem("token");
+            return "Old token";
+        }
+    } catch (err) {
+        throw Error(err);
+    }
+}
+export async function APItodos(category) {
+    const url = `http://localhost:3000/api/${category}/todos`;
+    const data = await setupFetch(url, "get");
+    // console.log(data);
+    return data;
+}
+export async function APIDetails(category, id) {
+    const url = `http://localhost:3000/api/${category}/${id}`;
+    const data = await setupFetch(url, "get");
+    return data;
+}
+export async function APIGetServiceBy(id, by) {
+    const url = `http://localhost:3000/api/servico/todos/${by}/${id}`;
+    const data = await setupFetch(url, "get");
+    return data;
+}
+
+export async function APIPostNewData(formData) {
+    const url = `http://localhost:3000/api/${formData["category"]}/novo`;
+    console.log(formData);
+    const data = await setupFetch(url, "post", formData);
+    // console.log(data);
+    return data;
+}
+export async function APIPutData(formData, id) {
+    const url = `http://localhost:3000/api/${formData["category"]}/${id}/edit`;
+    // console.log(formData);
+    const data = await setupFetch(url, "put", formData);
+    // console.log(data);
+
+    return data;
+}
+export async function ApiOneExcel(id) {
+    const url = `http://localhost:3000/api/export/${id}`;
+    const reqConfig = {
+        method: "Get",
+        headers: {
+            Authorization: "Bearer " + localStorage["token"],
+        },
+    };
+
+    try {
+        const response = await fetch(url, reqConfig);
+        if (response.status === 200) {
+            const blob = await response.blob();
+            // console.log(data);
+            return blob;
+        }
+    } catch (err) {
+        throw Error(err);
+    }
+}
+
+export async function ApiMonthExcel(id, date) {
+    const url = `http://localhost:3000/api/export/${id}/${date}`;
+    const reqConfig = {
+        method: "Get",
+        headers: {
+            Authorization: "Bearer " + localStorage["token"],
+        },
+    };
+
+    try {
+        const response = await fetch(url, reqConfig);
+        if (response.status === 200) {
+            const blob = await response.blob();
+            // console.log(data);
+            return blob;
+        }
+    } catch (err) {
+        throw Error(err);
+    }
+}
---
+++ src/utils.js
@@ -0,0 +1,142 @@
+export function downloadExcelAction(blob, fileName) {
+    const downloadLink = document.createElement("a");
+    downloadLink.download = fileName;
+    downloadLink.href = URL.createObjectURL(blob);
+    downloadLink.click();
+    URL.revokeObjectURL(downloadLink.href);
+}
+
+export function formatCpf(cpf) {
+    let arr = String(cpf).split("");
+    let a = arr.map((n, indx) => {
+        if (indx === 2 || indx === 5) {
+            return n + ".";
+        }
+        if (indx === 8) {
+            return n + "-";
+        }
+        return n;
+    });
+    return a.toString().replaceAll(",", "");
+}
+export function formatTelefone(tel) {
+    let separator = 4;
+    if (tel === undefined) {
+        return;
+    }
+    if (tel.includes("-")) {
+        return tel;
+    }
+    if (tel.length > 8) {
+        separator = 5;
+    }
+    return `${tel.substring(0, separator)}-${tel.substring(separator)} `;
+}
+
+export function booleanToString(b) {
+    if (b === true) {
+        return "Sim";
+    } else {
+        return "Não";
+    }
+}
+export function fullName(nome, sobrenome) {
+    if (!sobrenome) {
+        return nome;
+    } else {
+        return `${nome} ${sobrenome}`;
+    }
+}
+export function stateDetails(data, type) {
+    const local = () => {
+        const { nome, endereço, cep, telefone } = data.local;
+        const infoContent = {
+            Nome: nome,
+            Endereço: endereço,
+            CEP: cep,
+            Telefone: telefone,
+            Tabela: data.local.tabela,
+        };
+
+        const formState = {
+            nome,
+            cep,
+            endereço,
+            telefone,
+            category: "local",
+            formType: "edit",
+            tabela: data.local.tabela,
+            dbId: data.local._id,
+        };
+        return { infoContent, formState };
+    };
+    const dentista = () => {
+        const { nome, sobrenome, cpf, telefone, local } = data.dentista;
+        const infoContent = {
+            Nome: fullName(nome, sobrenome),
+            CPF: formatCpf(cpf),
+            Telefone: formatTelefone(telefone),
+            Franquia: local.nome,
+            Endereço: local.endereço,
+        };
+        const formState = {
+            nome,
+            sobrenome,
+            cpf: formatCpf(cpf),
+            telefone,
+            local: local._id,
+            category: "dentista",
+            formType: "edit",
+            dbId: data.dentista._id,
+        };
+        return { infoContent, formState };
+    };
+
+    const servico = () => {
+        const { statusEntrega, paciente } = data.serviço;
+        const { nome, sobrenome, _id, local } = data.serviço.dentista;
+        const produtoNomes = data.serviço.produto
+            .map((p, indx) => {
+                if (indx === 0) {
+                    return p.nome;
+                } else return " " + p.nome;
+            })
+            .toString();
+
+        const infoContent = {
+            Paciente: paciente,
+            Dentista: fullName(nome, sobrenome),
+            Produtos: produtoNomes,
+            Finalizado: booleanToString(statusEntrega),
+        };
+        const formState = {
+            paciente: paciente,
+            dentista: _id, //just id to be pre-selected at select input
+            local,
+            produto: data.serviço.produto,
+            statusEntrega,
+            category: "servico",
+            formType: "edit",
+            dbId: data.serviço._id,
+        };
+        // console.log(data, formState);
+        return { infoContent, formState };
+    };
+    const obj = {
+        servico,
+        local,
+        dentista,
+    };
+    return obj[type]();
+}
+
+// sort normal ascending
+export function sortBy(key, data) {
+    const ascending = data.sort((a, b) =>
+        a[key] > b[key] ? 1 : b[key] > a[key] ? -1 : 0
+    );
+    const descending = data.sort((a, b) =>
+        a[key] < b[key] ? 1 : b[key] < a[key] ? -1 : 0
+    );
+    return { ascending, descending };
+}
