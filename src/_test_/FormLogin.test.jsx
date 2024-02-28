010f31c4f640cc05c0d066fc172489583dca28d2
Gibsongf <maccgiiibson@gmail.com>
2024-02-28 01:45:58 -0300

    FormLogin test: call api & don't call with empty required element

--- src/_test_/FormLogin.test.jsx
+++ src/_test_/FormLogin.test.jsx
@@ -4,18 +4,26 @@ import { expect, vi } from "vitest";
 import userEvent from "@testing-library/user-event";
 import { HashRouter } from "react-router-dom";
 import App from "../App";
+import { apiLogin } from "../Api";
 
-//to confirm the form call the login api
-let loginCall = false;
-vi.mock("../Api", () => {
-    return {
-        default: { apiLogin: vi.fn() },
-        apiLogin: vi.fn(() => {
-            loginCall = true;
-        }),
-    };
-});
+//mock api post login
+vi.mock("../Api");
+
 describe("App component", () => {
+    //all useful elements of the form
+    const getInput = () => {
+        const username = screen.getByRole("textbox", {
+            name: "Nome de Usuário:",
+        });
+        const password = screen.getByLabelText("Senha:");
+        const button = screen.getByRole("button", { name: "Entrar" });
+        return { username, password, button };
+    };
+    //event user keyboard input
+    const inputText = async (el, text, user) => {
+        await user.dblClick(el);
+        await user.keyboard(text);
+    };
     it("renders form correct when no token registered", () => {
         const { container } = render(
             //HashRouter for useNavigate to work
@@ -27,7 +35,8 @@ describe("App component", () => {
         expect(container).toMatchSnapshot();
         expect(form).toBeInTheDocument();
     });
-    it("Form Login successfully", async () => {
+
+    it("call APIlogin successfully & register input correctly", async () => {
         const user = userEvent.setup();
         render(
             //HashRouter for useNavigate to work
@@ -35,20 +44,31 @@ describe("App component", () => {
                 <App />
             </HashRouter>
         );
-
-        const username = screen.getByRole("textbox", {
-            name: "Nome de Usuário:",
-        });
-        const password = screen.getByLabelText("Senha:");
-        const button = screen.getByRole("button", { name: "Entrar" });
-        await user.tripleClick(username);
-        await user.keyboard("test");
-        await user.tripleClick(password);
-        await user.keyboard("test");
+        let { username, password, button } = getInput();
+        await inputText(username, "test", user);
+        await inputText(password, "test", user);
         await user.click(button);
+
+        //input register correctly
         expect(username.value).toBe("test");
         expect(password.value).toBe("test");
         //confirm that the API is being called
-        expect(loginCall).toBeTruthy();
+        expect(apiLogin).toBeCalledTimes(1);
+        expect(apiLogin).toBeCalledWith({ username: "test", password: "test" });
+    });
+    it("Dont call ApiLogin with empty required element", async () => {
+        const user = userEvent.setup();
+        render(
+            //HashRouter for useNavigate to work
+            <HashRouter>
+                <App />
+            </HashRouter>
+        );
+        let { username, button } = getInput();
+        await inputText(username, "test", user);
+        await user.click(button);
+
+        //confirm that the API was called once in the previous test
+        expect(apiLogin).toBeCalledTimes(1);
     });
 });
